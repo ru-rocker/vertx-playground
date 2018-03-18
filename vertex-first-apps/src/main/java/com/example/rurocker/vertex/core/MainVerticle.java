25aa219f4f67d6fe81254537457d6f398a6b9558
ru.rocker81@gmail.com <ru.rocker81@gmail.com>
2018-03-18 21:06:49 +0700

    use router instead of direct handler

--- vertex-first-apps/src/main/java/com/example/rurocker/vertex/core/MainVerticle.java
+++ vertex-first-apps/src/main/java/com/example/rurocker/vertex/core/MainVerticle.java
@@ -2,15 +2,24 @@ package com.example.rurocker.vertex.core;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
 	@Override
 	public void start(Future<Void> fut) throws Exception {
+
+		// get port from configuration.
 		Integer port = config().getInteger("http.port", 8080);
-		vertx.createHttpServer().requestHandler(req -> {
-			req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
-		}).listen(port, result -> {
+
+		// Create a router object.
+		Router router = Router.router(vertx);
+		router.route("/").handler(routingContext -> {
+			routingContext.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
+		});
+
+		// create HTTP server.
+		vertx.createHttpServer().requestHandler(router::accept).listen(port, result -> {
 			if (result.succeeded()) {
 				fut.complete();
 			} else {
